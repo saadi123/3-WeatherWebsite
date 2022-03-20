68b4f583211d2d0521d9903bfbd2c14466e94b5c
Saad <saadpaki@gmail.com>
2022-03-21 02:22:40 +0500

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const { hasSubscribers } = require('diagnostics_channel');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
+const port = process.env.PORT || 3000;
 const app = express();
 const publicDirectory = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
@@ -91,6 +92,6 @@ app.get('*', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('The server is up on port 3000');
+app.listen(port, () => {
+    console.log('The server is up on port ' + port);
 });
